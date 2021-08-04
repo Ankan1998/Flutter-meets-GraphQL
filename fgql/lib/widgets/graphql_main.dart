eb6974c277f68d1b9c85d33b35f40b395b61986b
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-04 23:50:53 +0530

    Added value notifier to notify listener from change in the graph client

--- fgql/lib/widgets/graphql_main.dart
+++ fgql/lib/widgets/graphql_main.dart
@@ -1,3 +1,4 @@
+import 'package:fgql/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
@@ -14,8 +15,17 @@ class GraphMain extends StatefulWidget {
 class _GraphMainState extends State<GraphMain> {
   @override
   Widget build(BuildContext context) {
+    // httplink conatins the api endpoint
     final HttpLink httplink = HttpLink("https://api.github.com/graphql",
         defaultHeaders: {"authorization": "Bearer ${widget.access_token}"});
+
+    // Adding listeners
+    ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+        GraphQLClient(link: httplink, cache: GraphQLCache()));
     
+    return GraphQLProvider(
+      client: client,
+      child: HomeScreen()
+    );
   }
 }
