00e1145e4f930417e9e932e2a1337c5e08270e7b
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-04 23:34:50 +0530

    home screen started

---
+++ fgql/lib/screens/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:fgql/themes.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  //const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Gitty',
+          style: MyTheme.kAppTitle,
+        ),
+        elevation: 0,
+      ),
+      backgroundColor: MyTheme.kPrimaryColor,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.01
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )
+                ),
+                child: Container()
+              ),
+            ),
+          ],
+        ),
+      ),
+        
+      
+    );
+  }
+}
