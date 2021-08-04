7a4a78af2d93c9b26d37cb9251567226ae09da7f
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-04 21:14:20 +0530

    themes added

--- fgql/lib/main.dart
+++ fgql/lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:fgql/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      
+      theme: ThemeData(
+        primaryColor: MyTheme.kPrimaryColor,
+        accentColor: MyTheme.kAccentColor,
+        textTheme: GoogleFonts.merriweatherTextTheme(
+          //Theme.of(context).textTheme,
+        ),
+        //primarySwatch: Colors.blue[400],
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: SplashScreen(),
     )
   );
 }
