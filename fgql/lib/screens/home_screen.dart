f973719a4c525d1795742b59acd115455948b650
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-05 23:11:16 +0530

    Created user profile section

--- fgql/lib/screens/home_screen.dart
+++ fgql/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fgql/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   //const HomeScreen({ Key? key }) : super(key: key);
@@ -12,21 +13,80 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Gitty',
-          style: MyTheme.kAppTitle,
-        ),
-        elevation: 0,
-      ),
-      backgroundColor: MyTheme.kPrimaryColor,
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   backgroundColor: Colors.grey[900],
+      //   title: Text(
+      //     'Gitty',
+      //     style: TextStyle(
+      //       fontSize: 32,
+      //       fontWeight: FontWeight.w300,
+      //       letterSpacing: 4,
+      //       fontFamily: GoogleFonts.pacifico().fontFamily
+      //     ),
+      //   ),
+      //   elevation: 0,
+      // ),
+      backgroundColor: Colors.grey[900],
       body: Padding(
         padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
         child: Column(
           children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height*0.01
+              height: MediaQuery.of(context).size.height*0.05
+            ),
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(36.0),
+                  child: Container(
+                    width: 400,
+                    height:300,
+                    decoration: BoxDecoration(
+                                
+                      
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        stops: [
+                          0.1,
+                          0.3,
+                          0.8,
+                        ],
+                        colors: [
+                          Colors.yellow,
+                          Colors.orange[300],
+                          Colors.red[300],
+                        ],
+                      )
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Hello Gradient!',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top:15,
+                  left:15,
+                  child: CircleAvatar(
+                    radius:55,
+                    backgroundColor: Colors.red[900],
+                  ),
+                ),
+
+              ]
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.05
             ),
             Expanded(
               child: Container(
