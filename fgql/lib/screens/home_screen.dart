2fd373b2555b9732761e8fbde47a438bfe2a42bb
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-06 20:49:32 +0530

    Query builder added and user data is fetched

--- fgql/lib/screens/home_screen.dart
+++ fgql/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:fgql/themes.dart';
 import 'package:fgql/widgets/user_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   //const HomeScreen({ Key? key }) : super(key: key);
@@ -11,35 +12,85 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String readRepositories = """
+    query Flutter_Github_GraphQL{
+            user(login: "Ankan1998") {
+                avatarUrl(size: 200)
+                location
+                name
+                url
+                email
+                login
+                repositories {
+                  totalCount
+                }
+                followers {
+                  totalCount
+                }
+                following {
+                  totalCount
+                }
+              }
+      viewer {
+              starredRepositories(last: 12) {
+                edges {
+                  node {
+                    id
+                    name
+                    nameWithOwner
+                  }
+                }
+              }
+            }
+          }
+      """;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      body: SafeArea(
-        child: Column(
-          children: [          
-            UserDetail(),
-            SizedBox(
-              height: MediaQuery.of(context).size.height*0.01
-            ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  )
-                ),
-                child: Container()
+      body: Query(
+          options: QueryOptions(document: gql(readRepositories)),
+          builder: (QueryResult result,
+              {VoidCallback refetch, FetchMore fetchMore}) {
+            //print("ckbjmlckb");
+            //print(result.data['user']);
+            //print(result.hasException); 
+            //print(result.hasException);
+            // if (result.hasException != null) {
+            //   return Center(
+            //       child: Text(
+            //     result.hasException.toString(),
+            //     style: TextStyle(fontSize: 16),
+            //     textAlign: TextAlign.center,
+            //   ));
+            // }
+            if (result.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final userDetail = result.data['user'];
+            //print("dgjkfsdg");
+            //print("yo yo ${userDetail['avatarUrl']}");
+            return SafeArea(
+              child: Column(
+                children: [
+                  UserDetail(u_detail: userDetail),
+                  //UserDetail(),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+                  Expanded(
+                    child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            )),
+                        child: Container()),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ),
-        
-      
+            );
+          }),
     );
   }
 }
