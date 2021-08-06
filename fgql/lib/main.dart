ed4363ae6536038d2855dc807e75a8c195e214d2
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-06 20:48:17 +0530

    hive initialized

--- fgql/lib/main.dart
+++ fgql/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:fgql/screens/entry_screen.dart';
 import 'package:fgql/themes.dart';
+import 'package:fgql/widgets/graphql_main.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
+void main() async {
+  await initHiveForFlutter();
   runApp(
     MaterialApp(
       theme: ThemeData(
@@ -15,7 +18,7 @@ void main() {
         //primarySwatch: Colors.blue[400],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: EntryScreen(),
+      home: GraphMain(),
     )
   );
 }
