dd9671cb091e85ea5af352127a3af4fe6e600103
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-06 20:50:08 +0530

    graphQl client storage and code updated

--- fgql/lib/widgets/graphql_main.dart
+++ fgql/lib/widgets/graphql_main.dart
@@ -4,9 +4,7 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphMain extends StatefulWidget {
   //const GraphMain({ Key? key }) : super(key: key);
-  const GraphMain({Key key, this.access_token}) : super(key: key);
-
-  final String access_token;
+  //const GraphMain({Key key, this.access_token}) : super(key: key);
 
   @override
   _GraphMainState createState() => _GraphMainState();
@@ -15,17 +13,19 @@ class GraphMain extends StatefulWidget {
 class _GraphMainState extends State<GraphMain> {
   @override
   Widget build(BuildContext context) {
+    final String access_token = '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';
     // httplink conatins the api endpoint
-    final HttpLink httplink = HttpLink("https://api.github.com/graphql",
-        defaultHeaders: {"authorization": "Bearer ${widget.access_token}"});
+    final HttpLink httplink = HttpLink("https://api.github.com/graphql");
+    //defaultHeaders: {"authorization": "Bearer ${access_token}"});
+    final AuthLink authLink =
+        AuthLink(getToken: () async => "Bearer ${access_token}");
+    final Link link = authLink.concat(httplink);
+    
 
     // Adding listeners
     ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
-        GraphQLClient(link: httplink, cache: GraphQLCache()));
-    
-    return GraphQLProvider(
-      client: client,
-      child: HomeScreen()
-    );
+        GraphQLClient(link: link, cache: GraphQLCache(store: HiveStore())));
+
+    return GraphQLProvider(client: client, child: HomeScreen());
   }
 }
