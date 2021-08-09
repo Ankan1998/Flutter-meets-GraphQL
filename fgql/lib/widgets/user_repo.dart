2a706f29857235daa0ba4ec5146b24891281ffe3
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-08-09 17:52:34 +0530

    UI change in user_repo.dart

--- fgql/lib/widgets/user_repo.dart
+++ fgql/lib/widgets/user_repo.dart
@@ -20,77 +20,105 @@ class _UserRepoState extends State<UserRepo> {
       appBar: AppBar(
         title: Text("REPOSITORIES"),
         centerTitle: true,
+        backgroundColor: Colors.deepPurple[200] ,
+        elevation: 0,
       ),
+      backgroundColor: Colors.deepPurple[200],
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView.builder(
+        padding: const EdgeInsets.only(left:8.0,right:8.0),
+        child: Container(
           
-          itemCount: widget.repo.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Card(
-                shadowColor: Colors.grey[900],
-                child: ExpansionTile(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        child: widget.repo[index]['node']['description'] != null ? 
-                      Text(
-                        widget.repo[index]['node']['description'],
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey[700],
-                          fontFamily: GoogleFonts.lato().fontFamily,
-                          letterSpacing: 1.3
-                        )
-                      )
-                      : Text(
-                        "NULL",
-                      )
-                      ),
-                    )
+          padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  stops: [
+                    0.1,
+                    0.4,
+                    0.8,
                   ],
-                  title: Text(
-                    widget.repo[index]['node']['name'],
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.brown[600],
-                      fontFamily: GoogleFonts.courgette().fontFamily,
-                      letterSpacing: 1.3
-                    )
-                  ),
-                  trailing: Padding(
-                    padding: const EdgeInsets.only(top:10.0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                          KoukiconsFavourite2(height: 25,width: 25,),
-                          SizedBox(height:3),
-                          Text("${widget.repo[index]['node']['stargazerCount']}")
-                        ],),
-                        SizedBox(width:5),
-                        Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                          KoukiconsFlowChart(height: 25,width: 25,),
-                          SizedBox(height:3),
-                          Text("${widget.repo[index]['node']['forkCount']}")
-                        ],),
-                      ],
-                    ),
-                  ),
-              
+                  colors: [
+                    Colors.lightBlue[300].withOpacity(0.3),
+                    Colors.indigo[300].withOpacity(0.4),
+                    Colors.deepPurple[400].withOpacity(0.5),
+                  ],
                 ),
-              ),
-            );
-          },
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            )
+          ),
+          child: ListView.builder(
+            
+            itemCount: widget.repo.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Card(
+                  color: Colors.grey[200],
+                  shadowColor: Colors.grey[900],
+                  child: ExpansionTile(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: widget.repo[index]['node']['description'] != null ? 
+                        Text(
+                          widget.repo[index]['node']['description'],
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[700],
+                            fontFamily: GoogleFonts.lato().fontFamily,
+                            letterSpacing: 1.3
+                          )
+                        )
+                        : Text(
+                          "NULL",
+                        )
+                        ),
+                      )
+                    ],
+                    title: Text(
+                      widget.repo[index]['node']['name'],
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.brown[600],
+                        fontFamily: GoogleFonts.courgette().fontFamily,
+                        letterSpacing: 1.3
+                      )
+                    ),
+                    trailing: Padding(
+                      padding: const EdgeInsets.only(top:10.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                            KoukiconsFavourite2(height: 25,width: 25,),
+                            SizedBox(height:3),
+                            Text("${widget.repo[index]['node']['stargazerCount']}")
+                          ],),
+                          SizedBox(width:5),
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                            KoukiconsFlowChart(height: 25,width: 25,),
+                            SizedBox(height:3),
+                            Text("${widget.repo[index]['node']['forkCount']}")
+                          ],),
+                        ],
+                      ),
+                    ),
+                
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
