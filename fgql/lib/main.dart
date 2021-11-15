b61f517b44953f005eabef2cdf1c2a8a59dc7188
Ankan1998 <ankanroni3@gmail.com>
2021-11-15 23:19:47 +0530

    http certificate exception solved

--- fgql/lib/main.dart
+++ fgql/lib/main.dart
@@ -1,11 +1,22 @@
+import 'dart:io';
+
 import 'package:fgql/screens/entry_screen.dart';
 import 'package:fgql/themes.dart';
-import 'package:fgql/services/graphql_main.dart';
+import 'package:fgql/components/graphql_main.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() async {
+  HttpOverrides.global = new MyHttpOverrides();
   await initHiveForFlutter();
   runApp(
     MaterialApp(
